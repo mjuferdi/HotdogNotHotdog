f5d6cee06be295a00daeb942a4ab7d981f67356b
mjuferdi <juferdi.mario@yahoo.com>
2018-05-29 01:46:42 +0200

    Creating UI, Setup to use phone camera

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -7,19 +7,43 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+// TODO: 1. Delegate the controller
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    // TODO: 2. Create a new oject
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // TODO: 3. Set up image picker to allow user use front/back camera.
+        // Image cannot be edited, allowsEditing = false
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let userpickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = userpickedImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
     }
-
-
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
 }
 
